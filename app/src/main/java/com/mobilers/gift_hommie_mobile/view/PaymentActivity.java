41f844be6c1a6792e6936f5aab37d889a3c0953c
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-03-04 11:55:36 +0700

    #6 Payment screen

--- app/src/main/java/com/mobilers/gift_hommie_mobile/view/PaymentActivity.java
+++ app/src/main/java/com/mobilers/gift_hommie_mobile/view/PaymentActivity.java
@@ -2,13 +2,16 @@ package com.mobilers.gift_hommie_mobile.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mobilers.gift_hommie_mobile.R;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mobilers.gift_hommie_mobile.service.GlobalService;
 import com.paypal.checkout.approve.Approval;
 import com.paypal.checkout.approve.OnApprove;
 import com.paypal.checkout.createorder.CreateOrder;
@@ -30,15 +33,22 @@ import java.util.ArrayList;
 
 public class PaymentActivity extends AppCompatActivity {
     private static final String TAG = "PAYMENT: ";
-    PaymentButtonContainer paymentButtonContainer;
+    PaymentButtonContainer btnPaypal;
+    TextView btnCOD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        btnCOD = findViewById(R.id.btnCOD);
+        btnPaypal = findViewById(R.id.btnPaypal);
 
-        paymentButtonContainer = findViewById(R.id.payment_button_container);
+        btnCOD.setOnClickListener(v -> {
+//            GlobalService.getInstance().getCheckoutDTO().setPaymentMethod(1);
+            Intent intent = new Intent(PaymentActivity.this, CompetePaymentActivity.class);
+            startActivity(intent);
+        });
 
-        paymentButtonContainer.setup(
+        btnPaypal.setup(
                 new CreateOrder() {
                     @Override
                     public void create(@NotNull CreateOrderActions createOrderActions) {
@@ -67,11 +77,13 @@ public class PaymentActivity extends AppCompatActivity {
                 new OnApprove() {
                     @Override
                     public void onApprove(@NotNull Approval approval) {
+                        Intent intent = new Intent(PaymentActivity.this, CompetePaymentActivity.class);
+                        startActivity(intent);
                         approval.getOrderActions().capture(new OnCaptureComplete() {
                             @Override
                             public void onCaptureComplete(@NotNull CaptureOrderResult result) {
                                 Log.d(TAG, String.format("CaptureOrderResult: %s", result));
-                                Toast.makeText(PaymentActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(PaymentActivity.this, "Thanh toán thành công!", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
