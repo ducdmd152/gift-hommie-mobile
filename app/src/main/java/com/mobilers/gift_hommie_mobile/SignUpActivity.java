cf414361ade376cad368aaddbc3a64e493b1ae22
vtavta30920 <nghechngoc36@gmail.com>
2024-03-05 07:33:57 +0700

    Change UI

--- app/src/main/java/com/mobilers/gift_hommie_mobile/SignUpActivity.java
+++ app/src/main/java/com/mobilers/gift_hommie_mobile/SignUpActivity.java
@@ -1,7 +1,15 @@
 package com.mobilers.gift_hommie_mobile;
 
 
-public class SignUpActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
 
