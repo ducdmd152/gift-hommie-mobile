d7488947dab221171a1324705c847f96e549968f
thuanisuka <117148244+thuanisuka@users.noreply.github.com>
2024-03-19 00:02:42 +0700

    change

--- app/src/main/java/com/mobilers/gift_hommie_mobile/App.java
+++ app/src/main/java/com/mobilers/gift_hommie_mobile/App.java
@@ -1,5 +1,9 @@
 package com.mobilers.gift_hommie_mobile;
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.os.Build;
 
 import com.paypal.checkout.PayPalCheckout;
 import com.paypal.checkout.config.CheckoutConfig;
@@ -9,9 +13,11 @@ import com.paypal.checkout.createorder.UserAction;
 
 public class App extends Application {
 
+
     @Override
     public void onCreate() {
         super.onCreate();
+        createNotificationChannel();
         PayPalCheckout.setConfig(new CheckoutConfig(
                 this,
                 "ARoi3O0eCaY4PgNsrZxTJklW9GbaWekKLptBbN6PXhZ4US6fIYkInRUJ65X93zScKp1pyZSCLLqDTZqx",
@@ -20,5 +26,34 @@ public class App extends Application {
                 UserAction.PAY_NOW,
                 "com.mobilers.gifthommiemobile://paypalpay"
         ));
+    }
+    public static final String CHANNEL_ID_1 = "NOTIFICATION_1";
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+
+            //notification
+            CharSequence name = getString(R.string.notification);
+            String description = getString(R.string.notification_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel1 = new NotificationChannel(CHANNEL_ID_1, name, importance);
+            channel1.setDescription(description);
+
+
+
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null){
+                notificationManager.createNotificationChannel(channel1);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/mobilers/gift_hommie_mobile/view/CompetePaymentActivity.java
+++ app/src/main/java/com/mobilers/gift_hommie_mobile/view/CompetePaymentActivity.java
@@ -1,24 +1,52 @@
 package com.mobilers.gift_hommie_mobile.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.mobilers.gift_hommie_mobile.App;
 import com.mobilers.gift_hommie_mobile.R;
 
+import java.util.Date;
+
 public class CompetePaymentActivity extends AppCompatActivity {
+    private static final String TITLE_PUSH = "Đã đặt hàng";
+    private static final String CONTENT_PUSH = "Đon hàng sẽ sóm được giao đến bạn";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compete_payment);
 
+        senNotification();
+
         LinearLayout btnBackToHome = findViewById(R.id.btnBackToHome);
         btnBackToHome.setOnClickListener(v -> {
             Intent intent = new Intent(CompetePaymentActivity.this, ProductListActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void senNotification() {
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_ID_1)
+                .setContentTitle(TITLE_PUSH)
+                .setContentText(CONTENT_PUSH)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify( getNotificationID(),notification);
+        }
+
+
+    }
+    private int getNotificationID(){
+        return (int) new Date().getTime();
     }
 }
